d360d4ff7908ed170019825b6df5e0d09d1c2909
Akatsuki <lsx499285437@163.com>
2018-06-15 16:21:45 +0800

    add lpd algorithm test

--- app/main.cpp
+++ app/main.cpp
@@ -11,148 +11,153 @@
 
 #include "ippe/ippe.h"
 
+#include "lpe.h"
+
 using namespace std;
 using namespace cv;
 using namespace apriltag;
+using namespace vipad;
 
-void getRelativeTransform(double tag_size, double fx, double fy, double px, double py, double p[4][2]);
-void getRelativeTransform2(double tag_size, double fx, double fy, double cx, double cy, double p[4][2]);
-
-int main(int argc, char *argv[])
-{
-    // Initialize camera
-    VideoCapture cap(0);
-    if (!cap.isOpened()) {
-        cerr << "Couldn't open video capture device" << endl;
-        return -1;
-    }
-
-    // Initialize tag detector with options
-    apriltag_family_t *tf = NULL;
-    
-    tf = tags_create(tag36h11);
-
-    apriltag_detector_t *td = apriltag_detector_create();
-    apriltag_detector_add_family(td, tf);
-    
-    td->quad_decimate = 1.0f;
-    td->quad_sigma = 0.8f;
-    td->refine_edges = 1;
-    
-    
-
-    Mat frame, gray;
-    while (true) {
-        cap >> frame;
-        cvtColor(frame, gray, COLOR_BGR2GRAY);
-
-        // Make an image_u8_t header for the Mat data
-        image_u8_t im = { .width = gray.cols,
-            .height = gray.rows,
-            .stride = gray.cols,
-            .buf = gray.data
-        };
-
-        zarray_t *detections = apriltag_detector_detect(td, &im);
-        cout << zarray_size(detections) << " tags detected" << endl;
-
-        // Draw detection outlines
-        for (int i = 0; i < zarray_size(detections); i++) {
-            apriltag_detection_t *det;
-            zarray_get(detections, i, &det);
-            line(frame, Point(det->p[0][0], det->p[0][1]),
-                     Point(det->p[1][0], det->p[1][1]),
-                     Scalar(0, 0xff, 0), 2);
-            line(frame, Point(det->p[0][0], det->p[0][1]),
-                     Point(det->p[3][0], det->p[3][1]),
-                     Scalar(0, 0, 0xff), 2);
-            line(frame, Point(det->p[1][0], det->p[1][1]),
-                     Point(det->p[2][0], det->p[2][1]),
-                     Scalar(0xff, 0, 0), 2);
-            line(frame, Point(det->p[2][0], det->p[2][1]),
-                     Point(det->p[3][0], det->p[3][1]),
-                     Scalar(0xff, 0, 0), 2);
-
-            stringstream ss;
-            ss << det->id;
-            String text = ss.str();
-            int fontface = FONT_HERSHEY_SCRIPT_SIMPLEX;
-            double fontscale = 1.0;
-            int baseline;
-            Size textsize = getTextSize(text, fontface, fontscale, 2,
-                                            &baseline);
-            putText(frame, text, Point(det->c[0]-textsize.width/2,
-                                       det->c[1]+textsize.height/2),
-                    fontface, fontscale, Scalar(0xff, 0x99, 0), 2);
-        }
-        
-        /* -------------------------------------------------------------------- */
-        if (zarray_size(detections) > 0) {
-            apriltag_detection_t *det;
-            zarray_get(detections, 0, &det);
-            
-//             matd_t *pose = homography_to_pose(det->H, 2.4908279215754123e+03, 2.4935314568583112e+03, 3.4745731382095448e+02, 2.4094331871742105e+02);
+// void getRelativeTransform(double tag_size, double fx, double fy, double px, double py, double p[4][2]);
+// void getRelativeTransform2(double tag_size, double fx, double fy, double cx, double cy, double p[4][2]);
+// 
+// int main(int argc, char *argv[])
+// {
+//     // Initialize camera
+//     VideoCapture cap(0);
+//     if (!cap.isOpened()) {
+//         cerr << "Couldn't open video capture device" << endl;
+//         return -1;
+//     }
+// 
+//     // Initialize tag detector with options
+//     apriltag_family_t *tf = NULL;
+//     
+//     tf = tags_create(tag36h11);
+// 
+//     apriltag_detector_t *td = apriltag_detector_create();
+//     apriltag_detector_add_family(td, tf);
+//     
+//     td->quad_decimate = 1.0f;
+//     td->quad_sigma = 0.8f;
+//     td->refine_edges = 1;
+//     
+//     
+// 
+//     Mat frame, gray;
+//     while (true) {
+//         cap >> frame;
+//         cvtColor(frame, gray, COLOR_BGR2GRAY);
+// 
+//         // Make an image_u8_t header for the Mat data
+//         image_u8_t im = { .width = gray.cols,
+//             .height = gray.rows,
+//             .stride = gray.cols,
+//             .buf = gray.data
+//         };
+// 
+//         zarray_t *detections = apriltag_detector_detect(td, &im);
+//         cout << zarray_size(detections) << " tags detected" << endl;
+// 
+//         // Draw detection outlines
+//         for (int i = 0; i < zarray_size(detections); i++) {
+//             apriltag_detection_t *det;
+//             zarray_get(detections, i, &det);
+//             line(frame, Point(det->p[0][0], det->p[0][1]),
+//                      Point(det->p[1][0], det->p[1][1]),
+//                      Scalar(0, 0xff, 0), 2);
+//             line(frame, Point(det->p[0][0], det->p[0][1]),
+//                      Point(det->p[3][0], det->p[3][1]),
+//                      Scalar(0, 0, 0xff), 2);
+//             line(frame, Point(det->p[1][0], det->p[1][1]),
+//                      Point(det->p[2][0], det->p[2][1]),
+//                      Scalar(0xff, 0, 0), 2);
+//             line(frame, Point(det->p[2][0], det->p[2][1]),
+//                      Point(det->p[3][0], det->p[3][1]),
+//                      Scalar(0xff, 0, 0), 2);
+// 
+//             stringstream ss;
+//             ss << det->id;
+//             String text = ss.str();
+//             int fontface = FONT_HERSHEY_SCRIPT_SIMPLEX;
+//             double fontscale = 1.0;
+//             int baseline;
+//             Size textsize = getTextSize(text, fontface, fontscale, 2,
+//                                             &baseline);
+//             putText(frame, text, Point(det->c[0]-textsize.width/2,
+//                                        det->c[1]+textsize.height/2),
+//                     fontface, fontscale, Scalar(0xff, 0x99, 0), 2);
+//         }
+//         
+//         /* -------------------------------------------------------------------- */
+//         if (zarray_size(detections) > 0) {
+//             apriltag_detection_t *det;
+//             zarray_get(detections, 0, &det);
 //             
-// //             cout << "x y z : " << MATD_EL(pose, 0, 3) * 0.05 <<  ",\t" << MATD_EL(pose, 1, 3) * 0.05 << ",\t" << MATD_EL(pose, 2, 3) * 0.05 << endl;
-// //             cout << "R : " << MATD_EL(pose, 0, 0) <<  ",\t" << MATD_EL(pose, 0, 1) << ",\t" << MATD_EL(pose, 0, 2) << "\r\n"
-// //                            << MATD_EL(pose, 1, 0) <<  ",\t" << MATD_EL(pose, 1, 1) << ",\t" << MATD_EL(pose, 1, 2) << "\r\n"
-// //                            << MATD_EL(pose, 2, 0) <<  ",\t" << MATD_EL(pose, 2, 1) << ",\t" << MATD_EL(pose, 2, 2) << endl;
-// //             cout << "yaw :" << std::atan2<float>(MATD_EL(pose, 1, 0), MATD_EL(pose, 0, 0)) * 57.3 << endl;
+// //             matd_t *pose = homography_to_pose(det->H, 2.4908279215754123e+03, 2.4935314568583112e+03, 3.4745731382095448e+02, 2.4094331871742105e+02);
+// //             
+// // //             cout << "x y z : " << MATD_EL(pose, 0, 3) * 0.05 <<  ",\t" << MATD_EL(pose, 1, 3) * 0.05 << ",\t" << MATD_EL(pose, 2, 3) * 0.05 << endl;
+// // //             cout << "R : " << MATD_EL(pose, 0, 0) <<  ",\t" << MATD_EL(pose, 0, 1) << ",\t" << MATD_EL(pose, 0, 2) << "\r\n"
+// // //                            << MATD_EL(pose, 1, 0) <<  ",\t" << MATD_EL(pose, 1, 1) << ",\t" << MATD_EL(pose, 1, 2) << "\r\n"
+// // //                            << MATD_EL(pose, 2, 0) <<  ",\t" << MATD_EL(pose, 2, 1) << ",\t" << MATD_EL(pose, 2, 2) << endl;
+// // //             cout << "yaw :" << std::atan2<float>(MATD_EL(pose, 1, 0), MATD_EL(pose, 0, 0)) * 57.3 << endl;
+// //             
+// //             matd_t *R = matd_select(pose, 0, 2, 0, 2);
+// // //             cout << "R **** : " << MATD_EL(R, 0, 0) <<  ",\t" << MATD_EL(R, 0, 1) << ",\t" << MATD_EL(R, 0, 2) << "\r\n"
+// // //                         << MATD_EL(R, 1, 0) <<  ",\t" << MATD_EL(R, 1, 1) << ",\t" << MATD_EL(R, 1, 2) << "\r\n"
+// // //                         << MATD_EL(R, 2, 0) <<  ",\t" << MATD_EL(R, 2, 1) << ",\t" << MATD_EL(R, 2, 2) << endl;
+// //             
+// //             matd_t *t_ = matd_select(pose, 0, 2, 3, 3);
+// // //             cout << "x y z $$$$$$$ : " << MATD_EL(t_, 0, 0) * 0.05 <<  ",\t" << MATD_EL(t_, 1, 0) * 0.05 << ",\t" << MATD_EL(t_, 2, 0) * 0.05 << endl;
+// //             matd_t *R_t = matd_transpose(R);
+// // //             cout << "R ^^^^ : " << MATD_EL(R_t, 0, 0) <<  ",\t" << MATD_EL(R_t, 0, 1) << ",\t" << MATD_EL(R_t, 0, 2) << "\r\n"
+// // //                                 << MATD_EL(R_t, 1, 0) <<  ",\t" << MATD_EL(R_t, 1, 1) << ",\t" << MATD_EL(R_t, 1, 2) << "\r\n"
+// // //                                 << MATD_EL(R_t, 2, 0) <<  ",\t" << MATD_EL(R_t, 2, 1) << ",\t" << MATD_EL(R_t, 2, 2) << endl;
+// //             matd_t *t = matd_multiply(R_t, t_);
+// // //             cout << "size :::::::::::: " << t->nrows << ", " << t->ncols << endl;;
+// //             
+// //             cout << "x y z **** : " << MATD_EL(t, 0, 0) * 0.05 <<  ",\t" << MATD_EL(t, 1, 0) * 0.05 << ",\t" << MATD_EL(t, 2, 0) * 0.05 << endl;
+// //             
+// // //             cout << "goodness : " << det->goodness << endl;
+// //             
+// //             matd_destroy(R);
+// //             matd_destroy(t_);
+// //             matd_destroy(t);
+// //             matd_destroy(R_t);
+// //             matd_destroy(pose);
 //             
-//             matd_t *R = matd_select(pose, 0, 2, 0, 2);
-// //             cout << "R **** : " << MATD_EL(R, 0, 0) <<  ",\t" << MATD_EL(R, 0, 1) << ",\t" << MATD_EL(R, 0, 2) << "\r\n"
-// //                         << MATD_EL(R, 1, 0) <<  ",\t" << MATD_EL(R, 1, 1) << ",\t" << MATD_EL(R, 1, 2) << "\r\n"
-// //                         << MATD_EL(R, 2, 0) <<  ",\t" << MATD_EL(R, 2, 1) << ",\t" << MATD_EL(R, 2, 2) << endl;
+//             chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
+//             getRelativeTransform(0.1, 2.4908279215754123e+03, 2.4935314568583112e+03, 3.4745731382095448e+02, 2.4094331871742105e+02, det->p);
+//             chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
+//             chrono::duration<double> time_used = chrono::duration_cast<chrono::duration<double>> (t2 - t1);
+//             std::cout << "opencv pnp cost time: " << time_used.count() << std::endl;
 //             
-//             matd_t *t_ = matd_select(pose, 0, 2, 3, 3);
-// //             cout << "x y z $$$$$$$ : " << MATD_EL(t_, 0, 0) * 0.05 <<  ",\t" << MATD_EL(t_, 1, 0) * 0.05 << ",\t" << MATD_EL(t_, 2, 0) * 0.05 << endl;
-//             matd_t *R_t = matd_transpose(R);
-// //             cout << "R ^^^^ : " << MATD_EL(R_t, 0, 0) <<  ",\t" << MATD_EL(R_t, 0, 1) << ",\t" << MATD_EL(R_t, 0, 2) << "\r\n"
-// //                                 << MATD_EL(R_t, 1, 0) <<  ",\t" << MATD_EL(R_t, 1, 1) << ",\t" << MATD_EL(R_t, 1, 2) << "\r\n"
-// //                                 << MATD_EL(R_t, 2, 0) <<  ",\t" << MATD_EL(R_t, 2, 1) << ",\t" << MATD_EL(R_t, 2, 2) << endl;
-//             matd_t *t = matd_multiply(R_t, t_);
-// //             cout << "size :::::::::::: " << t->nrows << ", " << t->ncols << endl;;
+//             t1 = chrono::steady_clock::now();
+//             getRelativeTransform2(0.1, 2.4908279215754123e+03, 2.4935314568583112e+03, 3.4745731382095448e+02, 2.4094331871742105e+02, det->p);
+//             t2 = chrono::steady_clock::now();
+//             chrono::duration<double> time_used2 = chrono::duration_cast<chrono::duration<double>> (t2 - t1);
+//             std::cout << "ippe cost time: " << time_used2.count() << std::endl;
 //             
-//             cout << "x y z **** : " << MATD_EL(t, 0, 0) * 0.05 <<  ",\t" << MATD_EL(t, 1, 0) * 0.05 << ",\t" << MATD_EL(t, 2, 0) * 0.05 << endl;
+//             std::cout << "ippe speed / opencv speed :" << time_used.count() / time_used2.count() << endl;
 //             
-// //             cout << "goodness : " << det->goodness << endl;
+//             std::cout << "center : " << det->c[0] << ", " << det->c[1] << std::endl;
 //             
-//             matd_destroy(R);
-//             matd_destroy(t_);
-//             matd_destroy(t);
-//             matd_destroy(R_t);
-//             matd_destroy(pose);
-            
-            chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
-            getRelativeTransform(0.1, 2.4908279215754123e+03, 2.4935314568583112e+03, 3.4745731382095448e+02, 2.4094331871742105e+02, det->p);
-            chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
-            chrono::duration<double> time_used = chrono::duration_cast<chrono::duration<double>> (t2 - t1);
-            std::cout << "opencv pnp cost time: " << time_used.count() << std::endl;
-            
-            t1 = chrono::steady_clock::now();
-            getRelativeTransform2(0.1, 2.4908279215754123e+03, 2.4935314568583112e+03, 3.4745731382095448e+02, 2.4094331871742105e+02, det->p);
-            t2 = chrono::steady_clock::now();
-            chrono::duration<double> time_used2 = chrono::duration_cast<chrono::duration<double>> (t2 - t1);
-            std::cout << "ippe cost time: " << time_used2.count() << std::endl;
-            
-            std::cout << "ippe speed / opencv speed :" << time_used.count() / time_used2.count() << endl;
-            
-            apriltag_detection_destroy(det);
-        }
-        
-        zarray_destroy(detections);
-
-        imshow("Tag Detections", frame);
-        if (waitKey(1) == 27)
-            break;
-    }
-
-    apriltag_detector_destroy(td);
-    
-    tags_destroy(tf);
-
-    return 0;
-}
+//             apriltag_detection_destroy(det);
+//         }
+//         
+//         zarray_destroy(detections);
+// 
+//         imshow("Tag Detections", frame);
+//         if (waitKey(1) == 27)
+//             break;
+//     }
+// 
+//     apriltag_detector_destroy(td);
+//     
+//     tags_destroy(tf);
+// 
+//     return 0;
+// }
 
 void getRelativeTransform(double tag_size, double fx, double fy, double px, double py, double p[4][2])
 {
@@ -239,11 +244,63 @@ void getRelativeTransform2(double tag_size, double fx, double fy, double cx, dou
     matd_destroy(cameraMatrix);
     matd_destroy(distParam);
     matd_destroy(tinv);
+    matd_destroy(R);
+    matd_destroy(t);
 }
 
 
 
+int main(int argc, char **argv)
+{
+    // Initialize camera
+    VideoCapture cap(0);
+    if (!cap.isOpened()) {
+        cerr << "Couldn't open video capture device" << endl;
+        return -1;
+    }
+    
+    vipad::lpe_params param;
+    localPosition locat;
+    
+    param.angle = ClockwiseAngle_270;
+    
+    param.fx = 2.4908279215754123e+03;
+    param.fy = 2.4935314568583112e+03;
+    param.cx = 3.4745731382095448e+02;
+    param.cy = 2.4094331871742105e+02;
+    
+    param.k1 = -5.0968287369808518e-02;
+    param.k2 = -8.0252844113471298e+01;
+    param.p1 = -1.5334326534795978e-03;
+    param.p2 = -1.8098396142340031e-02;
+    param.k3 = -1.0045140113684745e+00;
+    
+    param.tag_type = tag36h11;
+    param.marker_length = 0.1f;
+    
+    param.q = NULL;
+    param.locat = &locat;
 
+    Mat frame, gray;
+    while (true) {
+        cap >> frame;
+        cvtColor(frame, gray, COLOR_BGR2GRAY);
+        
+        param.width = gray.cols;
+        param.height = gray.rows;
+        param.input = gray.data;
+        
+        static LocalPositionEstimation est(&param);
+        
+        est.estimateLocalPosition();
+
+        imshow("Tag Detections", frame);
+        if (waitKey(1) == 27)
+            break;
+    }
+    
+    return 0;
+}
 
 
 
